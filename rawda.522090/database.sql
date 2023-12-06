27f1f2040acdb3289cabd98d14c281d3e84e2cab
Rawdaalyaly <rawdaali210@gmail.com>
2023-12-06 10:33:50 +0200

    hello rawda

---
+++ rawda.522090/database.sql
@@ -0,0 +1,15 @@
+create table user_2
+(
+id int primary key,
+usernames_ varchar (50) ,
+passworddd int ,
+gender varchar (50) ,
+Age int  ,
+phone int ,
+city varchar (60) ,
+);
+insert into user_2 values 
+(1, 'rawda' , 0124 ,'female' , 19 , 1113465 , 'egypt') ,
+(2, 'ali' , 0125 ,'male' , 20 , 1223465 , 'germany') ,
+(3, 'ahmed' , 0126 , 'male' , 20 , 1453465 , 'spain');
+select * from user_2; 
